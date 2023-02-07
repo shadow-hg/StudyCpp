a22b58e8e1ddfcecafb286cbb29c5000fde7e32a
zhg <2503701047@qq.com>
2023-02-08 00:41:07 +0800

    dev:增加测试文件

---
+++ class01.h
@@ -0,0 +1,36 @@
+﻿//小练习
+#pragma once
+#include <iostream>
+
+namespace namespace_Class01
+{
+    class class01
+    {
+    private:
+        std::string name = "zhanghg";
+        int age = 23;
+        float height = 175;
+
+    public:
+        void hw()
+        {
+            std::string myText = "HelloWorld";
+            std::cout << myText << std::endl;
+        }
+
+        void getName()
+        {
+            std::cout << name <<std::endl;
+        }
+
+        void getAge()
+        {
+            std::cout << age;
+        }
+
+        void getHeight()
+        {
+            std::cout << height;
+        }
+    };
+}
