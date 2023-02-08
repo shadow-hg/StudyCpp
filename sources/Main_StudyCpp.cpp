2069e46d88d92fa0f2d959180a1c201e3a9ac40c
shadow-hg <61582640+shadow-hg@users.noreply.github.com>
2023-02-08 10:18:42 +0000

    dev:类的调用

--- sources/Main_StudyCpp.cpp
+++ sources/Main_StudyCpp.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
 using namespace std;
+#include "class01.cpp"
+
+class011 saaa;
 
 int main()
 {
-    
-    return 0;
+    saaa.my01();
+    cout << endl;
+    class011::my01();
 }
-
--- sources/class01.cpp
+++ sources/class01.cpp
@@ -2,17 +2,28 @@
 #include <iostream>
 using namespace namespace_Class01;
 
-class new_peo
+class class011
 {
-public:
-    void my01()
-    {
-        class01 people01;
-        std::cout << "姓名：" << std::endl;
-        people01.getName();
-        std::cout << "年龄：" << std::endl;
-        people01.getAge();std::cout << "岁" << std::endl;
-        std::cout << "身高：" << std::endl;
-        people01.getHeight();std::cout << "cm" <<std::endl;
-    }
+    public:
+        static void my01()
+        {
+            class01 people01;
+            std::cout << "姓名：";
+            people01.getName();
+            std::cout << "年龄：";
+            people01.getAge();std::cout << "岁" << std::endl;
+            std::cout << "身高：";
+            people01.getHeight();std::cout << "cm" <<std::endl;
+        }
+    private:
+        void my02()
+        {
+            class01 people01;
+            std::cout << "姓名：" << std::endl;
+            people01.getName();
+            std::cout << "年龄：" << std::endl;
+            people01.getAge();std::cout << "岁" << std::endl;
+            std::cout << "身高：" << std::endl;
+            people01.getHeight();std::cout << "cm" <<std::endl;
+        }
 };
